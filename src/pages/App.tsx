ae5a1748cca7bc0abe6214279324c695113e55b8
Mateusz Szostek <szostekm230590@gmail.com>
2020-12-18 22:16:30 +0000

    Initial commit
    Basic files created. React-router, react-router-dom, framer-motion, aos@next intaled, gatsby-cli installed

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {Route, Switch} from "react-router-dom";
+import AOS from "aos";
+import 'aos/dist/aos.css';
+import { AnimatePresence, motion} from 'framer-motion';
+import '../css/styles.css'
+
+export default () => {
+  
+  return (
+    <React.Fragment>
+
+      
+    </React.Fragment>
+  )
+}
