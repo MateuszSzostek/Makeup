37e894d854be957c0b5a6ad053b612122559f513
Mateusz Szostek <szostekm230590@gmail.com>
2020-12-26 20:41:09 +0000

    Home Page Update
    Gallery shortcut added, contact form added, footer added

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,8 @@
 import React from "react"
-import {useLocation, Route, Switch, Link, BrowserRouter as Router} from "react-router-dom";
+import { useLocation, Route, Switch, Link, BrowserRouter as Router } from "react-router-dom";
 import AOS from "aos";
 import 'aos/dist/aos.css';
-import { AnimatePresence, motion} from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import '../css/styles.css'
 import Home from './Home';
 import Offer from './Offer';
@@ -12,34 +12,37 @@ import FacebookImg from '../img/facebook.svg';
 import InstagramImg from '../img/instagram.svg';
 import PinterestImg from '../img/pinterest.svg';
 
-function App () {
+function App() {
   let location = useLocation();
   let linksData = [
-    {id:1, link:"/", page:"HOME"},
-    {id:2, link:"/offer", page:"OFFER"},
-    {id:3, link:"/portfolio", page:"PORTFOLIO"},
-    {id:4, link:"/contact", page:"CONTACT"},
+    { id: 1, link: "/", page: "Home" },
+    { id: 2, link: "/offer", page: "Offer" },
+    { id: 3, link: "/portfolio", page: "Portfolio" },
+    { id: 4, link: "/contact", page: "Contact" },
   ];
   let links = linksData.map(s => <Link key={s.id} to={s.link}>{s.page}</Link>)
   let routes = linksData.map(s => <Route key={s.id} path={s.link}></Route>);
 
   const pageTransition = {
     in: {
-        y: "0px",
-        opacity: 1,
-        duration: 2,
-        ease: "ease",
+      y: "0px",
+      opacity: 1,
+      duration: 2,
+      ease: "ease",
     },
     out: {
-        y: "-5px",
-        opacity: 0,
-        duration: 2,
-        ease: "ease",
-        },
-    }
+      y: "-5px",
+      opacity: 0,
+      duration: 2,
+      ease: "ease",
+    },
+  }
 
   return (
-
+    <React.Fragment>
+      <div className="background-text">
+          <h1>Makeup</h1>
+      </div>
     <div className="app-container flex-container justify-content-center">
       <div className="page-container">
         <div className="nav-container flex-container row justify-content-between">
@@ -53,32 +56,40 @@ function App () {
             </div>
           </div>
           <div className="social-media row flex-container justify-content-center align-items-center">
-              <img src={FacebookImg}></img>
-              <img src={InstagramImg}></img>
-              <img src={PinterestImg}></img>
+            <img src={FacebookImg}></img>
+            <img src={InstagramImg}></img>
+            <img src={PinterestImg}></img>
           </div>
         </div>
-        <div className="col flex-container align-items-center"> 
-        <AnimatePresence exitBeforeEnter> 
-          <Switch location={location} key={location.pathname}>
-            <Route exact path="/">
-              <Home transition={pageTransition}/>
-            </Route>
-            <Route path="/offer">
-              <Offer transition={pageTransition}/>
-            </Route>
-            <Route path="/portfolio">
-              <Portfolio transition={pageTransition}/>
-            </Route>
-            <Route path="/contact">
-              <Contact transition={pageTransition}/>
-            </Route>
-          </Switch>
-        </AnimatePresence>
+        <div className="col flex-container align-items-center">
+          <AnimatePresence exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route exact path="/">
+                <Home transition={pageTransition} />
+              </Route>
+              <Route path="/offer">
+                <Offer transition={pageTransition} />
+              </Route>
+              <Route path="/portfolio">
+                <Portfolio transition={pageTransition} />
+              </Route>
+              <Route path="/contact">
+                <Contact transition={pageTransition} />
+              </Route>
+            </Switch>
+          </AnimatePresence>
+        </div>
       </div>
-      </div>
-
     </div>
+    <div className="bottom-links flex-container align-items-center justify-content-center">
+      <div className="row">
+        {links}
+      </div>
+    </div>
+    <div>
+      <footer className="footer">Created with passion by Mateusz Szostek</footer>
+  </div>
+  </React.Fragment>
     /*
     <div className="flex-container col align-items-center">
       <div className="flex-container col app-container">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,13 @@ import React from "react"
 import 'aos/dist/aos.css';
 import '../css/styles.css';
 import LandingImg from '../img/landingImg.jpg';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
-export default function Home({transition}) {
-  return(
+export default function Home({ transition }) {
+  return (
     <React.Fragment>
       <motion.div initial="out" animate="in" exit="out" variants={transition}>
-        <div className="content-container"> 
+        <div>
           <div className="home-container flex-container row">
             <div className="home-image flex-container col">
               <img src={LandingImg}></img>
@@ -17,7 +17,47 @@ export default function Home({transition}) {
               <h2>Anna Dorsch</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget aliquet elit. Vivamus rhoncus tincidunt aliquam. Sed et mauris ut ex commodo faucibus. Integer sit amet porta felis. Aliquam pulvinar, odio eget dignissim egestas, lacus tortor dignissim urna, at ultricies ipsum quam pulvinar mi. Integer feugiat ante odio, id semper ipsum fermentum eget. Mauris a erat maximus, pulvinar sapien eu, hendrerit massa.</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget aliquet elit. Vivamus rhoncus tincidunt aliquam. Sed et mauris ut ex commodo faucibus. Integer sit amet porta felis. Aliquam pulvinar, odio eget dignissim egestas, lacus tortor dignissim urna, at ultricies ipsum quam pulvinar mi. Integer feugiat ante odio, id semper ipsum fermentum eget. Mauris a erat maximus, pulvinar sapien eu, hendrerit massa.</p>
-              <button className="btn">Contact Me</button>
+              <button className="btn mt-30">Contact Me</button>
+            </div>
+          </div>
+          <div className="example-gallery">
+            <a href={LandingImg} className="image"><img src={LandingImg} alt=""></img></a>
+            <a href={LandingImg} className="image"><img src={LandingImg} alt=""></img></a>
+            <a href={LandingImg} className="image"><img src={LandingImg} alt=""></img></a>
+            <a href={LandingImg} className="image"><img src={LandingImg} alt=""></img></a>
+          </div>
+          <div className="content-container flex-container row justify-content-between align-items-center contact-decoration mb-120 mt-60">
+            <div className="">
+              <form action="" className="contact-form flex-container col justify-content-center align-items-center">
+                <h1>Let's meet together</h1>
+                <div className="flex-container col">
+                  <label >Name</label>
+                  <input type="text" id="name" name="name" placeholder="What's Your name?"  ></input>
+                </div>
+                <div className="flex-container col" >
+                  <label >Email address</label>
+                  <input type="text" id="email" name="email" placeholder="What's Your email address?" ></input>
+                </div>
+                <div className="flex-container col">
+                  <label >Message</label>
+                  <textarea name="text" id="text" placeholder="What's Your message?"></textarea>
+                </div>
+                <button className="btn mt-15">Send</button>
+                <div className="center-all flex-container  col info-container">
+                  <div className="spin-container" >
+                    <div className="loading-spinner"></div>
+                  </div>
+                  <div className="message-info "></div>
+                </div>
+              </form>
+            </div>
+            <div className="contact-info flex-container justify-content-center align-items-center">
+              <div>
+                <h1>Anna Dorsch</h1>
+                <p>Email Address</p>
+                <p>+ 48 0378509378</p>
+                <p>08:00 - 15:00</p>
+              </div>
             </div>
           </div>
         </div>
