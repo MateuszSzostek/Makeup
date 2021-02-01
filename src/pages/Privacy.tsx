8c55d5f7e9775f171a425b43e21c594e0af3d5ba
Mateusz Szostek <szostekm230590@gmail.com>
2021-02-01 23:16:09 +0000

    Update 1.01v
    Footer mobile size changed, Privacy updated

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -12,7 +12,7 @@ let Privacy = () => {
             Privacy Policy for 4BeautyMakeup
         </h3>
           <p className="">
-            At ANNAWEBSITENAME, accessible from www.anna.uk.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by ANNAWEBSITENAME and how we use it.
+            At www.4beautymakeup.com, accessible from www.anna.uk.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by www.4beautymakeup.com and how we use it.
         </p>
           <p className="">
             If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us. Our Privacy Policy was generated with the help of GDPR Privacy Policy Generator from GDPRPrivacyNotice.com
@@ -70,22 +70,22 @@ let Privacy = () => {
                 Log Files
           </li>
             </ul>
-          ANNAWEBSITENAME follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+            www.4beautymakeup.com follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
         </p>
 
           <p className="">
             <h3>Privacy Policies</h3>
-          You may consult this list to find the Privacy Policy for each of the advertising partners of ANNAWEBSITENAME.
+          You may consult this list to find the Privacy Policy for each of the advertising partners of www.4beautymakeup.com.
         </p>
           <p className="">
-            Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on ANNAWEBSITENAME, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+            Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on www.4beautymakeup.com, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
         </p>
           <p className="">
-            Note that ANNAWEBSITENAME has no access to or control over these cookies that are used by third-party advertisers.
+            Note that www.4beautymakeup.com has no access to or control over these cookies that are used by third-party advertisers.
         </p>
           <p className="">
             <h3>Third Party Privacy Policies</h3>
-          ANNAWEBSITENAME's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
+            www.4beautymakeup.com's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
         </p>
           <p className="">
             You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
@@ -97,11 +97,11 @@ let Privacy = () => {
 
         </p>
           <p className="">
-            ANNAWEBSITENAME does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
+          www.4beautymakeup.com does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
         </p>
           <p className="">
             <h3>Online Privacy Policy Only</h3>
-          Our Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in ANNAWEBSITENAME. This policy is not applicable to any information collected offline or via channels other than this website.
+          Our Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in www.4beautymakeup.com. This policy is not applicable to any information collected offline or via channels other than this website.
         </p><p className="">
             <h3>Consent</h3>
           By using our website, you hereby consent to our Privacy Policy and agree to its terms.
